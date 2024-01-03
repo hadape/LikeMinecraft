89bc156ea06d8cae796ba077759c6e5ff6e92a00
p.hadacova <p.hadacova@gmail.com>
2024-01-03 17:50:48 +0100

    Pause menu

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -1,14 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class Menu : MonoBehaviour
 {
+    public static bool gamePaused = false;
+    [SerializeField]
+    private GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        gamePaused = true;
+        Time.timeScale = 0f;
+        pauseMenuUI.SetActive(true);
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void Resume()
+    {
+        gamePaused = false;
+        Time.timeScale = 1.0f;
+        pauseMenuUI.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     public void LoadGame()
     {
         SceneManager.LoadScene("Main");
+    }
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
     public void ExitGame()
     {
